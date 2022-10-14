d84a2e4e2e1e3f7e543d74ef3e8eed89bca1ccae
Devendra Singh <devnegikec@gmail.com>
2022-10-14 20:17:15 +0530

    updated main file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { getByTestId, render } from "@testing-library/react";
-import { App } from "./App";
+import React from 'react';
+import { getByTestId, render } from '@testing-library/react';
+import { App } from './App';
 
-describe('tests the app root component', function() {
-    it('should render the awesome text', function() {
+describe('tests the app root component', function () {
+    it('should render the awesome text', function () {
         render(<App />);
         // expect(getByTestId('awesom')).toBeInTheDocument();
-        expect(
-            getByTestId(document.documentElement, 'awesome2'),
-          ).toBeInTheDocument()
-    })
+        expect(getByTestId(document.documentElement, 'awesome2')).toBeInTheDocument();
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import image from './assets/image.jpg';
-import { ClickCounter } from './components/ClickCounter';
 import './App.css';
 
 export const App = () => {
     return (
         <div>
-           <div data-testid="awesome">awesome</div>
+            <div data-testid="awesome">awesome</div>
         </div>
     );
 };
