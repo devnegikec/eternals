8e0d90fa84747c1f32604b562c657ada45bc775e
Devendra Singh <devnegikec@gmail.com>
2022-10-14 20:42:16 +0530

    updated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,6 @@ describe('tests the app root component', function () {
     it('should render the awesome text', function () {
         render(<App />);
         // expect(getByTestId('awesom')).toBeInTheDocument();
-        expect(getByTestId(document.documentElement, 'awesome')).toBeInTheDocument();
+        expect(getByTestId(document.documentElement, 'awesome-again')).toBeInTheDocument();
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 export const App = () => {
     return (
         <div>
-            <div data-testid="awesome">awesome</div>
+            <div data-testid="awesome-again">awesome</div>
         </div>
     );
 };
