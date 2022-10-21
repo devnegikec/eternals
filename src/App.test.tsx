26768f0737990e74b13cfb88ab0799ae98f9e9c1
Devendra Singh <devnegikec@gmail.com>
2022-10-21 16:46:40 +0530

    adding unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,61 @@
 import React from 'react';
-import { getByTestId, render } from '@testing-library/react';
+import { getByTestId, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { App } from './App';
 
-describe('tests the app root component', function () {
-    it('should render the awesome text', function () {
+describe('test the app root component', () => {
+    it('should correctly display, the created items on the screen', async () => {
+        const user = userEvent.setup();
         render(<App />);
-        // expect(getByTestId('awesom')).toBeInTheDocument();
-        expect(getByTestId(document.documentElement, 'awesome-again')).toBeInTheDocument();
+        const itemDescription = screen.getByRole('textbox');
+        const createButton = screen.getByRole('button');
+
+        await user.click(itemDescription);
+        await user.keyboard('First item');
+        await user.click(createButton);
+
+        expect(itemDescription).toHaveValue('');
+        expect(screen.getByText('First item')).toBeInTheDocument();
+
+        await user.click(itemDescription);
+        await user.keyboard('Second item');
+        await user.click(createButton);
+
+        expect(itemDescription).toHaveValue('');
+        expect(screen.getByText('Second item')).toBeInTheDocument();
+    });
+
+    it('should not add empty item descriptions to the list', async () => {
+        const user = userEvent.setup();
+        render(<App />);
+        const itemDescription = screen.getByRole('textbox');
+        const createButton = screen.getByRole('button');
+
+        await user.click(itemDescription);
+        await user.keyboard('First item');
+        await user.click(createButton);
+
+        expect(itemDescription).toHaveValue('');
+        await user.click(createButton); // tries to create an item with empty description
+
+        expect(screen.getAllByRole('listitem')).toHaveLength(1);
+    });
+
+    it('should allow user to create items with the same descriptions', async () => {
+        const user = userEvent.setup();
+        render(<App />);
+        const itemDescription = screen.getByRole('textbox');
+        const createButton = screen.getByRole('button');
+
+        await user.click(itemDescription);
+        await user.keyboard('First item');
+        await user.click(createButton);
+
+        await user.click(itemDescription);
+        await user.keyboard('First item');
+        await user.click(createButton);
+
+        expect(screen.getAllByText('First item')).toHaveLength(2);
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { Input, Button } from '@mui/material';
 
 export const App = () => {
+    const [itemDescription, setItemDescription] = useState<string>('');
+    const [items, setItems] = useState<string[]>([]);
+
     return (
         <div>
-            <div data-testid="awesome-again">awesome</div>
+            <Input
+                value={itemDescription}
+                onChange={(event) => {
+                    event.preventDefault();
+                    setItemDescription(event.target.value);
+                }}
+            />
+            <Button
+                onClick={() => {
+                    if (itemDescription !== '') {
+                        setItems([...items, itemDescription]);
+                        setItemDescription('');
+                    }
+                }}
+            >
+                Create
+            </Button>
+            <ul>
+                {items.map((item) => {
+                    return <li key={item}>{item}</li>;
+                })}
+            </ul>
         </div>
     );
 };
